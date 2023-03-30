365b82ba171eda0abcf25110c566310d66aa28cf
JB Hutch <jbhutch01@gmail.com>
2023-03-30 15:03:11 -0400

    update label

--- src/components/Label/src/Label.tsx
+++ src/components/Label/src/Label.tsx
@@ -1,17 +1,22 @@
 import { cva } from 'class-variance-authority'
 import { type ClassValue } from 'class-variance-authority/dist/types'
+import clsx from 'clsx'
 import { Label, type LabelProps } from 'react-aria-components/src/Label'
 
 export interface _LabelProps extends Omit<LabelProps, 'className'> {
   className?: ClassValue
 }
 
-const labelVariants = cva(
-  'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-)
-
 const _Label = ({ className, ...props }: _LabelProps) => {
-  return <Label className={labelVariants({ className })} {...props} />
+  return (
+    <Label
+      className={clsx(
+        'mb-2 mr-3 block font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+        className
+      )}
+      {...props}
+    />
+  )
 }
 
 export { _Label as Label }
