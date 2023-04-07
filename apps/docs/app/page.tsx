e815ce269bf624454f79f7dcdf6d108765b1a3cf
JB Hutch <jbhutch01@gmail.com>
2023-04-07 19:17:32 -0400

    Updates

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import { allDocs } from 'contentlayer/generated'
-import { Button } from '@/../../packages/ui'
 
 export default async function Home() {
   return (
@@ -9,8 +8,6 @@ export default async function Home() {
         <span className="text-lg font-medium">Links</span>
       </div>
       <ul>
-        <Button>Hey</Button>
-
         {allDocs.map((doc, idx) => (
           <li key={idx}>
             <Link className="text-blue-500 underline" href={doc.slug}>
