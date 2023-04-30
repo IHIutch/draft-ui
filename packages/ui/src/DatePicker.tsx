c552d83e625f3e854f62ae402e7d691e30d58469
JB Hutch <jbhutch01@gmail.com>
2023-04-30 12:47:05 -0400

    Add enter/exit animations for calendar popovers

--- packages/ui/src/DatePicker.tsx
+++ packages/ui/src/DatePicker.tsx
@@ -1,10 +1,6 @@
-import { type ClassValue } from 'class-variance-authority/dist/types'
 import { DatePicker, Dialog, Popover } from 'react-aria-components'
 
-// export interface _MenuProps
-//   extends VariantProps<typeof buttonVariants> {
-//   className?: ClassValue
-// }
+import { cn } from '../lib/utils'
 
 const _DatePicker = (props) => {
   return <DatePicker {...props} />
@@ -12,8 +8,20 @@ const _DatePicker = (props) => {
 
 const _DatePickerContent = (props) => {
   return (
-    <Popover className="rounded-lg border border-slate-200 bg-white p-3 shadow-md">
-      <Dialog {...props} />
+    <Popover
+      className={cn(
+        'data-[entering]:animate-in data-[entering]:fade-in',
+        'data-[exiting]:animate-in data-[exiting]:fade-in data-[exiting]:direction-reverse',
+        'data-[placement=top]:slide-in-from-bottom-2',
+        'data-[placement=bottom]:slide-in-from-top-2'
+      )}
+    >
+      <Dialog
+        className={cn(
+          'rounded-lg border border-slate-200 bg-white p-3 shadow-md'
+        )}
+        {...props}
+      />
     </Popover>
   )
 }
--- packages/ui/src/DateRangePicker.tsx
+++ packages/ui/src/DateRangePicker.tsx
@@ -1,10 +1,6 @@
-import { type ClassValue } from 'class-variance-authority/dist/types'
 import { DateRangePicker, Dialog, Popover } from 'react-aria-components'
 
-// export interface _MenuProps
-//   extends VariantProps<typeof buttonVariants> {
-//   className?: ClassValue
-// }
+import { cn } from '../lib/utils'
 
 const _DateRangePicker = (props) => {
   return <DateRangePicker {...props} />
@@ -12,8 +8,20 @@ const _DateRangePicker = (props) => {
 
 const _DateRangePickerContent = (props) => {
   return (
-    <Popover>
-      <Dialog {...props} />
+    <Popover
+      className={cn(
+        'data-[entering]:animate-in data-[entering]:fade-in',
+        'data-[exiting]:animate-in data-[exiting]:fade-in data-[exiting]:direction-reverse',
+        'data-[placement=top]:slide-in-from-bottom-2',
+        'data-[placement=bottom]:slide-in-from-top-2'
+      )}
+    >
+      <Dialog
+        className={cn(
+          'rounded-lg border border-slate-200 bg-white p-3 shadow-md'
+        )}
+        {...props}
+      />
     </Popover>
   )
 }
