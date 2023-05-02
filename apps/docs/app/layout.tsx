3b36ec093ba9e5ec8e014bbea741497fd872207f
JB Hutch <jbhutch01@gmail.com>
2023-05-02 15:39:04 -0400

    Update docs to use 'use client' and SSRProvider

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import '@/styles/globals.css'
+
+import { SSRProvider } from 'react-aria-components'
 
 export default function RootLayout({
   children,
@@ -12,7 +16,9 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <SSRProvider>{children}</SSRProvider>
+      </body>
     </html>
   )
 }
--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,12 +1,62 @@
+'use client'
+
+import {
+  Calendar,
+  CalendarCell,
+  CalendarGrid,
+  CalendarGridBody,
+  CalendarGridHeader,
+  CalendarHeader,
+  CalendarHeaderCell,
+  CalendarHeading,
+  CalendarNextButton,
+  CalendarPrevButton,
+  DateInput,
+  DateInputGroup,
+  DatePicker,
+  DatePickerContent,
+  DateSegment,
+  IconButton,
+  Label,
+} from '@/../../packages/ui/src'
 import { allDocs } from 'contentlayer/generated'
+import { ChevronDown } from 'lucide-react'
 import Link from 'next/link'
 
-export default async function Home() {
+export default function Home() {
   return (
     <div>
       <div>
         <span className="text-lg font-medium">Links</span>
       </div>
+      <DatePicker>
+        <Label>Date</Label>
+        <DateInputGroup className="pr-1">
+          <DateInput>
+            {(segment) => <DateSegment segment={segment} />}
+          </DateInput>
+          <IconButton size="sm" variant="subtle" className="ml-auto shrink-0">
+            <ChevronDown size="16" strokeWidth="3" />
+          </IconButton>
+        </DateInputGroup>
+        <DatePickerContent>
+          <Calendar>
+            <CalendarHeader>
+              <CalendarPrevButton />
+              <CalendarHeading />
+              <CalendarNextButton />
+            </CalendarHeader>
+            <CalendarGrid>
+              <CalendarGridHeader>
+                {(day) => <CalendarHeaderCell>{day}</CalendarHeaderCell>}
+              </CalendarGridHeader>
+              <CalendarGridBody>
+                {(date) => <CalendarCell date={date} />}
+              </CalendarGridBody>
+            </CalendarGrid>
+          </Calendar>
+        </DatePickerContent>
+      </DatePicker>
       <ul>
         {allDocs.map((doc, idx) => (
           <li key={idx}>
