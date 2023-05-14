85046ede9b9ac1e37f24a195bad3b22c2795fb7f
JB Hutch <jbhutch01@gmail.com>
2023-05-14 16:53:13 -0400

    Fix nextjs update

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -7,15 +7,6 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-    // fontLoaders: [
-    //   {
-    //     loader: '@next/font/google',
-    //     options: { subsets: ['latin'] },
-    //   },
-    // ],
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
