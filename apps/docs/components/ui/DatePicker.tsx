eaa8d7ac16611568b8b2d11d86b531cf0ca15cb4
JB Hutch <jbhutch01@gmail.com>
2023-05-27 12:15:37 -0400

    Update datepicker

--- apps/docs/components/ui/DatePicker.tsx
+++ apps/docs/components/ui/DatePicker.tsx
@@ -1,14 +1,39 @@
 'use client'
 
-import { DatePicker, Dialog, Popover } from 'react-aria-components'
+import {
+  DatePicker,
+  Dialog,
+  Popover,
+  type DatePickerProps,
+  type DateValue,
+  type DialogProps,
+  type PopoverProps,
+} from 'react-aria-components'
 
 import { cn } from '@/lib/utils'
 
-const _DatePicker = (props) => {
+export interface _DatePickerProps<T extends DateValue>
+  extends Omit<DatePickerProps<T>, 'className'> {
+  className?: string
+}
+
+const _DatePicker = <T extends DateValue>(props: DatePickerProps<T>) => {
   return <DatePicker {...props} />
 }
 
-const _DatePickerContent = (props) => {
+export interface _DatePickerContentProps<T>
+  extends Omit<PopoverProps, 'children' | 'style' | 'className'>,
+    Omit<DialogProps, 'className'> {
+  value?: T
+  className?: string
+  popoverClassName?: string
+}
+
+const _DatePickerContent = <T extends object>({
+  className,
+  popoverClassName,
+  ...props
+}: _DatePickerContentProps<T>) => {
   return (
     <Popover
       className={cn(
@@ -17,10 +42,11 @@ const _DatePickerContent = (props) => {
         'data-[entering]:animate-in data-[entering]:fade-in',
         'data-[exiting]:animate-in data-[exiting]:fade-in data-[exiting]:direction-reverse',
         'data-[placement=top]:slide-in-from-bottom-2',
-        'data-[placement=bottom]:slide-in-from-top-2'
+        'data-[placement=bottom]:slide-in-from-top-2',
+        popoverClassName
       )}
     >
-      <Dialog className={cn('')} {...props} />
+      <Dialog className={className} {...props} />
     </Popover>
   )
 }
