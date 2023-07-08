59ecd3b923f7b295f2ae2de923dd9589e8521994
JB Hutch <jbhutch01@gmail.com>
2023-07-08 16:38:45 -0400

    Fix ComponentExample tabs

--- apps/docs/components/ComponentExample.tsx
+++ apps/docs/components/ComponentExample.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 import { Highlight, themes } from 'prism-react-renderer'
-import { Tab, TabList, TabPanel, TabPanels, Tabs } from 'ui'
+import { Tab, TabList, TabPanel, Tabs } from 'ui'
 
 import { cn } from '@/lib/utils'
 
@@ -22,57 +22,55 @@ export default function ComponentExample({
 
   return (
     <div>
-      <Tabs>
-        <TabList aria-label="History of Ancient Rome" disabledKeys={['Emp']}>
+      <Tabs disabledKeys={['Emp']}>
+        <TabList aria-label="History of Ancient Rome">
           <Tab id="example">Example</Tab>
           <Tab id="code">Code</Tab>
         </TabList>
-        <TabPanels>
-          <TabPanel id="example" className="px-0">
-            <div className="not-prose rounded-md border">
-              <div
-                className={cn('flex min-h-[350px] justify-center p-10', {
-                  'items-center': align === 'center',
-                  'items-start': align === 'start',
-                  'items-end': align === 'end',
-                })}
-              >
-                {Example}
-              </div>
+        <TabPanel id="example" className="px-0">
+          <div className="not-prose rounded-md border">
+            <div
+              className={cn('flex min-h-[350px] justify-center p-10', {
+                'items-center': align === 'center',
+                'items-start': align === 'start',
+                'items-end': align === 'end',
+              })}
+            >
+              {Example}
             </div>
-          </TabPanel>
-          <TabPanel id="code">
-            <div className="relative">
-              <div className="absolute right-2 top-2 z-10">
-                <CopyClipboardButton text={String(Code)} />
-              </div>
-              <Highlight
-                theme={themes.nightOwl}
-                code={String(Code)}
-                language="tsx"
-              >
-                {({
-                  // className,
-                  style,
-                  tokens,
-                  getLineProps,
-                  getTokenProps,
-                }) => (
-                  <pre style={style} className="my-0">
-                    {tokens.map((line, i) => (
-                      <div key={i} {...getLineProps({ line })}>
-                        {/* <span>{i + 1}</span> */}
-                        {line.map((token, key) => (
-                          <span key={key} {...getTokenProps({ token })} />
-                        ))}
-                      </div>
-                    ))}
-                  </pre>
-                )}
-              </Highlight>
+          </div>
+        </TabPanel>
+        <TabPanel id="code">
+          <div className="relative">
+            <div className="absolute right-2 top-2 z-10">
+              <CopyClipboardButton text={String(Code)} />
             </div>
-          </TabPanel>
-        </TabPanels>
+            <Highlight
+              theme={themes.nightOwl}
+              code={String(Code)}
+              language="tsx"
+            >
+              {({
+                // className,
+                style,
+                tokens,
+                getLineProps,
+                getTokenProps,
+              }) => (
+                <pre style={style} className="my-0">
+                  {tokens.map((line, i) => (
+                    <div key={i} {...getLineProps({ line })}>
+                      {/* <span>{i + 1}</span> */}
+                      {line.map((token, key) => (
+                        <span key={key} {...getTokenProps({ token })} />
+                      ))}
+                    </div>
+                  ))}
+                </pre>
+              )}
+            </Highlight>
+          </div>
+        </TabPanel>
       </Tabs>
     </div>
   )
