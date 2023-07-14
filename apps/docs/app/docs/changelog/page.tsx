f1886b6938845eab7134f6a43fd3a4a9895aac08
JB Hutch <jbhutch01@gmail.com>
2023-07-14 14:20:21 -0400

    Add changelog page

--- apps/docs/app/docs/changelog/page.tsx
+++ apps/docs/app/docs/changelog/page.tsx
@@ -1,5 +1,29 @@
 import React from 'react'
 
 export default function Changelog() {
-  return <div>Changelog</div>
+  return (
+    <div className="my-12 w-full">
+      <div className="prose prose-slate mx-auto">
+        <div>
+          <h1 className="mb-4">Changelog</h1>
+          <p className="lead mt-4">
+            Stay up to date on the latest changes and updates
+          </p>
+        </div>
+        <div>
+          <h2>July 2023</h2>
+          <p>
+            Using <code>react-aria-components@1.0.0-alpha.5</code>
+          </p>
+          <ul>
+            <li>We have a logo and a homepage!</li>
+            <li>
+              Docs updated; content pages are live including installation
+              instructions
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
