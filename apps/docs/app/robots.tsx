333fb13f820e203be599d26ebe9e28985ee1e2b3
JB Hutch <jbhutch01@gmail.com>
2023-08-28 10:55:26 -0400

    Add robots.txt

---
+++ apps/docs/app/robots.tsx
@@ -0,0 +1,12 @@
+import { type MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://draft-ui.vercel.app/sitemap.xml',
+  }
+}
