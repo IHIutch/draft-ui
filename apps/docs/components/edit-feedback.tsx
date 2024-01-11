11d556c94bfae9217fcce7d0918b558e33230b46
henrikvtcodes <commits@henrikvt.com>
2024-01-11 17:26:54 -0500

    Create feedback + edit link component

---
+++ apps/docs/components/edit-feedback.tsx
@@ -0,0 +1,36 @@
+type MinimalPost = {
+  title: string
+  slug: string
+}
+
+type Props = {
+  post: MinimalPost
+}
+
+export default function EditFeedbackLinks({ post }: Props) {
+  const pageEditUrl = `https://github.com/IHIutch/draft-ui/tree/main/apps${post.slug}`
+  const pageFeedbackUrl = new URL(
+    `https://github.com/IHIutch/draft-ui/issues/new`,
+  )
+  pageFeedbackUrl.searchParams.set('title', `Feedback for “${post.title}”`)
+  // Is this the right label to set? Do we need to make a new label for this?
+  pageFeedbackUrl.searchParams.set('labels', 'documentation')
+
+  return (
+    <span className="flex flex-col space-y-2">
+      <a
+        className="mt-6 text-xs text-slate-600 hover:text-slate-900 dark:text-slate-400 dark:hover:text-white"
+        href={pageEditUrl}
+      >
+        Edit this page
+      </a>
+      <a
+        className="mt-6 text-xs text-slate-600 hover:text-slate-900 dark:text-slate-400 dark:hover:text-white"
+        href={pageFeedbackUrl.toString()}
+        target="_blank"
+      >
+        Give us feedback
+      </a>
+    </span>
+  )
+}
